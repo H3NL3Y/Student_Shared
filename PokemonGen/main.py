f7fc6ba029db364fd7abd283db42ef8e9cb8a4dd
H3NL3Y <33662710+H3NL3Y@users.noreply.github.com>
2025-12-05 10:32:01 +0000

    Fix formatting and improve code readability
    Added functionality to write to external file.

--- PokemonGen/main.py
+++ PokemonGen/main.py
@@ -1,5 +1,6 @@
-#Pokemon Gen V1
+# Pokemon Gen V1
 import random
+
 
 def welcome():
     print("""
@@ -17,6 +18,7 @@ def welcome():
     WELCOME TO THE POKEDEX
     """)
 
+
 def menu():
     menuChoice = input("""
     Please select a choice :
@@ -32,23 +34,39 @@ def menu():
         pass
     else:
         print("Option not valid")
-   
-def  createPokemon():
+
+
+def createPokemon():
     name = input("Enter name: ")
-    HP = random.randint(1,100)
-    DEFENCE = random.randint(1,100)
-    SPEED = random.randint(1,100)
-    SPEEDATTACK = random.randint(1,100)
-    SPEEDDEFENCE = random.randint(1,100)
+    HP = random.randint(1, 100)
+    DEFENCE = random.randint(1, 100)
+    SPEED = random.randint(1, 100)
+    SPEEDATTACK = random.randint(1, 100)
+    SPEEDDEFENCE = random.randint(1, 100)
     print(f"HP {HP}")
     print(f"DEFENCE {DEFENCE}")
     print(f"SPEED {SPEED}")
     print(f"SPEEDATTACK {SPEEDATTACK}")
     print(f"SPEEDDEFENCE {SPEEDDEFENCE}")
-   
-    Types = ["Fire","Water","Ground","Rock","Poison","Fairy","Ice","dragon","flying","Ghost","Dark","Bug"]
+
+    Types = ["Fire", "Water", "Ground", "Rock", "Poison", "Fairy", "Ice", "dragon", "flying", "Ghost", "Dark", "Bug"]
     randomType = random.choice(Types)
     print(f"{name}'s type is: {randomType}")
-         
+
+#Friday 05/12/2025
+    pokeFile = open("pokemon.txt", "a")
+    pokeFile.write(f"Name: {name}\n")
+    pokeFile.write(f"HP: {HP}\n")
+    pokeFile.write(f"DEFENCE: {DEFENCE}\n")
+    pokeFile.write(f"SPEED: {SPEED}\n")
+    pokeFile.write(f"SPEEDATTACK: {SPEEDATTACK}\n")
+    pokeFile.write(f"SPEED DEFENCE: {SPEEDDEFENCE}\n")
+    pokeFile.write(f"TYPE: {randomType}\n")
+    pokeFile.close()
+
+
+
+
+
 welcome()
 menu()
